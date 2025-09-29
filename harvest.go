b8f84e42db14a455b5db5c86048675d90dddaff8
R. S. Doiel <rsdoiel@gmail>
2025-09-29 16:43:58 -0700

    commented out some debug statements

--- harvest.go
+++ harvest.go
@@ -274,9 +274,9 @@ func saveItem(db *sql.DB, feedLabel string, channel string, status string, item 
 	if item.Extensions != nil {
 		if source, ok := item.Extensions["source"]; ok && len(source) > 0 {
 			if data, ok := source["markdown"]; ok {
-				fmt.Printf("\nDEBUG source[%q] -> %T -> %+v\n", "markdown", data, data)
+				//fmt.Printf("\nDEBUG source[%q] -> %T -> %+v\n", "markdown", data, data)
 				for i, val := range data {
-					fmt.Printf("\nDEBUG source[%q][%d] -> %T -> %+v\n", "markdown", i, val, val)
+					//fmt.Printf("\nDEBUG source[%q][%d] -> %T -> %+v\n", "markdown", i, val, val)
 					if val.Name == "markdown" {
 						sourceMarkdown = val.Value
 					}
